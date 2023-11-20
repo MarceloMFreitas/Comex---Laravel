be1da19cf1a8ec1d2dea264d316c461bdb52ea41
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-11-20 16:44:41 -0300

    Create CategoriasController.php

---
+++ comex/app/Http/Controllers/CategoriasController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoriasController extends Controller
+{
+    public function index(){
+        $categorias=[
+            'CELULARES',
+            'INFORMÁTICA',
+            'MÓVEIS',
+            'AUTOMOTIVA',
+            'LIVROS',
+            'BELEZA',
+            'ESPORTE',
+            'LUXO'
+        ];
+        
+        return view('categorias.index')->with('categorias',$categorias ) ;
+
+    }
+}
