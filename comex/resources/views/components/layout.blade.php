b57ef97fb6d6000f34b2985f22a38eb6936cd939
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-11-20 19:13:27 -0300

    Update layout.blade.php

--- comex/resources/views/components/layout.blade.php
+++ comex/resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>{{$title}}</title>
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     <h1>{{$title}}</h1>
