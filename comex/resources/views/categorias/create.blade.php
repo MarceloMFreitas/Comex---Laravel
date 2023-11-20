958b8b000f82af98ccd84ce11c1912a39734f988
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-11-20 17:11:23 -0300

    Create create.blade.php

---
+++ comex/resources/views/categorias/create.blade.php
@@ -0,0 +1,8 @@
+<x-layout title="Nova Categoria">
+
+    <form action="" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome">
+    </form>
+
+</x-layout>
