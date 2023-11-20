d3c70086eec9429f8ac0b30c47cd3c37a38a3eb2
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-11-20 16:44:47 -0300

    Create index.blade.php

---
+++ comex/resources/views/categorias/index.blade.php
@@ -0,0 +1,11 @@
+<x-layout title="Categorias">
+
+    <ul>
+        @foreach ($categorias as $categoria )
+
+            <li>{{$categoria}}</li>    
+        
+        @endforeach
+    </ul>
+
+</x-layout>
