2d78d80eb78ad3c59c3c1d90710e88a0fe774957
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-11-25 10:51:09 -0300

    Update CategoriasController.php

--- comex/app/Http/Controllers/CategoriasController.php
+++ comex/app/Http/Controllers/CategoriasController.php
@@ -26,15 +26,14 @@ class CategoriasController extends Controller
        
         $categoria = Categoria::create($request->all());
 
-        session()->flash('mensagem.sucesso',"Categoria '$categoria->nome' adicionada com sucesso");
 
-        return to_route('categorias.index');
+        return to_route('categorias.index')->with('mensagem.sucesso',"Categoria '$categoria->nome' adicionada com sucesso");
     }
 
     public function destroy(Request $request, Categoria $categoria){
         
         $categoria->delete();
-        session()->flash('mensagem.sucesso',"Categoria '$categoria->nome' removida com sucesso");
-        return to_route('categorias.index');
+    
+        return to_route('categorias.index')->with('mensagem.sucesso',"Categoria '$categoria->nome' removida com sucesso");
     }
 }
