28700d30f6e2d0824850f2889623e2eb01443c47
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-11-25 17:38:42 -0300

    Update index.blade.php

--- comex/resources/views/categorias/index.blade.php
+++ comex/resources/views/categorias/index.blade.php
@@ -11,13 +11,14 @@
         @foreach ($categorias as $categoria )
 
             <li class="list-group-item d-flex justify-content-between align-items-center">{{$categoria->nome}}
-
-                <form action="{{ route('categorias.destroy', $categoria->id)}}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger btn-sm">X</button>
-                </form>
-
+                <span class="d-flex">
+                    <a href="{{route('categorias.edit', $categoria->id)}}" class="btn btn-primary btn-sm">E</a>
+                    <form action="{{ route('categorias.destroy', $categoria->id)}}" method="post" class="ms-2">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger btn-sm">X</button>
+                    </form>
+                </span>
             </li>    
         
         @endforeach
