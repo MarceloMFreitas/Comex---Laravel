a41e997c0b5a82a3fd62660fb2fd14cd04026c44
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-11-25 17:38:35 -0300

    Update create.blade.php

--- comex/resources/views/categorias/create.blade.php
+++ comex/resources/views/categorias/create.blade.php
@@ -1,12 +1,5 @@
 <x-layout title="Nova Categoria">
 
-    <form action="{{ route('categorias.store')}}" method="post">
-        @csrf
-        <div class="mb-5">
-            <label for="nome" class="form-label">Nome:</label>
-            <input type="text" id="nome" name="nome" class="form-control">
-        </div>
-        <button type="submit" class="btn btn-primary">Adicionar</button>
-    </form>
-
+    <x-form :action="route('categorias.store')"/>
+    
 </x-layout>
