310f5d13c2d533a9bdf1091c2f7daed6a601b2e1
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-11-28 09:20:18 -0300

    Create 2023_11_27_124601_create_clientes_table.php

---
+++ comex/database/migrations/2023_11_27_124601_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome', 128);
+            $table->unsignedBigInteger('cpf');
+            $table->string('telefone');
+            $table->unsignedBigInteger('endereco');
+            $table->foreign('endereco')->references('id')->on('endereco');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
